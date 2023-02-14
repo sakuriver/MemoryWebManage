7e49ddb292ce4bff1fc34fbd1af6daecf736052c
sakuriver <suzuerain@yahoo.co.jp>
2023-02-14 12:32:38 +0900

    メニューを整備
    大項目と配信用データの情報を設定

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("static/template/*.html")),
+	}
+
+	e.Renderer = t
+
+	e.GET("/top", TopHtml)
+
+	e.GET("/client_data_top", ClientDataTopHtml)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type DataForm struct{}
+
+func TopHtml(c echo.Context) error {
+	return c.Render(http.StatusOK, "top", DataForm{})
+}
+
+func ClientDataTopHtml(c echo.Context) error {
+	return c.Render(http.StatusOK, "client_data_top", DataForm{})
 }
